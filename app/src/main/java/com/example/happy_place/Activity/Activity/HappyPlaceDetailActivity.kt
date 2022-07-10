c6aeb36756dad844cd4ff4edec4e864f823e297a
Anshu <anshukumarsingh448@gmail.com>
2022-07-10 15:52:19 +0530

    first

---
+++ app/src/main/java/com/example/happy_place/Activity/Activity/HappyPlaceDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.happy_place.Activity.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.happy_place.R
+
+class HappyPlaceDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_happy_place_detail)
+    }
+}
